23c4c02c0ed2cfed6c105b430a01495dcc4a7d1e
ngdiho <hoangt2k24@gmail.com>
2020-09-17 01:28:16 +0700

    update

--- public/views/Home.php
+++ public/views/Home.php
@@ -206,15 +206,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     <!-- Bootstrap core JavaScript-->
     <script src="../lib/jquery/jquery-3.5.1.min.js"></script>
     <script src="../lib/bootstrap/js/bootstrap.min.js"></script>
-    <script src="../js/sidebar.js"></script>
 
-    <!-- Menu Toggle Script -->
-    <script>
-        $("#menu-toggle").click(function(e) {
-            e.preventDefault();
-            $("#wrapper").toggleClass("toggled");
-        });
-    </script>
 </body>
 
 </html>
